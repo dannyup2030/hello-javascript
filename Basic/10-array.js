36dd1aa3fabd568ac1a3885fb3b21cf44cee2855
dannyup2030 <dannyup8@hotmail.com>
2025-02-17 17:14:02 -0500

    x

--- Basic/10-array.js
+++ Basic/10-array.js
@@ -8,7 +8,7 @@ Vídeo: https://youtu.be/1glVfFxj8a4?t=8741
 // Declaración
 
 let myArray = []
-let myArray2 = new Array()
+let myArray2 = new Array() // De esta manera creamos un array en el cual le especificamos cuantos elmentos vamos a ingresat
 
 console.log(myArray)
 console.log(myArray2)
@@ -52,8 +52,8 @@ console.log(myArray)
 
 myArray = []
 
-// push y pop
-
+// push 
+//Agrega los elementos que mentamos dentro del parentesis en el array al final de la lista
 myArray.push("Brais")
 myArray.push("Moure")
 myArray.push("mouredev")
@@ -61,31 +61,34 @@ myArray.push(37)
 
 console.log(myArray)
 
+//Pop
 console.log(myArray.pop()) // Elimina el último y lo devuelve
 myArray.pop()
 
 console.log(myArray)
 
-// shift y unshift
-
+// shift 
+//Elimina el primer elemento del array y lo devuelve
 console.log(myArray.shift())
 console.log(myArray)
 
+// unshift
+//Agrega los elementos que le demos al principio del array
 myArray.unshift("Brais", "mouredev")
 console.log(myArray)
 
 // length
-
+// Con length podemos ver la longitud de nuestro array
 console.log(myArray.length)
 
 // clear
-
-myArray = []
+//De esta manera podemos borrar un array
+console.log(myArray = [])//De esta manera es preferible
 myArray.length = 0 // alternativa
 console.log(myArray)
 
 // slice
-
+//De esta manera le decimos con que elementos  queremos quedarnos del array 
 myArray = ["Brais", "Moure", "mouredev", 37, true]
 
 let myNewArray = myArray.slice(1, 3)
@@ -94,11 +97,12 @@ console.log(myArray)
 console.log(myNewArray)
 
 // splice
-
+//El splice elimina desde el primer indice que le demos hasta el segundo indice que le demos
 myArray.splice(1, 3)
 console.log(myArray)
 
 myArray = ["Brais", "Moure", "mouredev", 37, true]
 
+//De esta manera hacemos lo mismo con los dos indices y en el ultimo indice le indicamos si queremos agregar algo en el medio 
 myArray.splice(1, 2, "Nueva entrada")
 console.log(myArray)
--- Basic/13-structures-exercises.js
+++ Basic/13-structures-exercises.js
@@ -4,17 +4,32 @@ Vídeo: https://youtu.be/1glVfFxj8a4?t=11451
 */
 
 // 1. Crea un array que almacene cinco animales
-
+let array = ["leon","zebra","tigre","perro","gato"]
 // 2. Añade dos más. Uno al principio y otro al final
+array.push("conejo")
+array.unshift("avestruz")
+console.log(array)
 
 // 3. Elimina el que se encuentra en tercera posición
+array.splice(3,1)
+console.log(array)
 
 // 4. Crea un set que almacene cinco libros
 
+let newset = new Set(["padre rico","la culpa de la vaca","mejora habitos","el caballero de la amardura oxidada","animales"])
+console.log(newset)
+
 // 5. Añade dos más. Uno de ellos repetido
+newset.add("camino al exito")
+console.log(newset)
+newset.add("mejora habitos")
+console.log(newset)
 
 // 6. Elimina uno concreto a tu elección
+console.log(newset.delete("la culpa de la vaca"))
+console.log(newset)
 
+console.log(newset.size)
 // 7. Crea un mapa que asocie el número del mes a su nombre
 
 // 8. Comprueba si el mes número 5 existe en el map e imprime su valor
